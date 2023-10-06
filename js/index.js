ccf8aa01463e9ee8c514b0d2511cace07ffb7d7e
jahid <jahid1o@outlook.com>
2023-10-06 15:33:55 +0600

    Modify js/index.js  - created function game  - modified previous functions  - completed the game

--- js/index.js
+++ js/index.js
@@ -1,20 +1,54 @@
-function getComputerChoice(){
-    let choices = ['Rock', 'Paper', 'Scissors']
-    let choice = Math.floor(Math.random() * 3)
-    return choices[choice]
+
+function game(){
+    function getComputerChoice() {
+        let choices = ['Rock', 'Paper', 'Scissors'];
+        let choice = Math.floor(Math.random() * 3);
+        return choices[choice];
+    }
+    
+    function playSingleRound(playerSelection, computerSelection){
+        p = playerSelection.toLowerCase();
+        c = computerSelection.toLowerCase();
+    
+        if (p == c){
+            return 'D';
+        }
+        else if ((p == 'rock' && c == 'paper') || (p == 'paper' && c == 'scissors') || (p == 'scissors' && c == 'rock')){
+            return 'L';
+        }
+        else{
+            return 'W';
+        }
+    }
+    
+    let playerScore = 0, computerScore = 0;
+
+    for (let round = 0; round < 5; round++){
+        let userChoice;
+        while (true) {
+            userChoice = prompt(`Round ${round+1}.\nYour turn. Chose either rock, paper or scissors.`).toLocaleLowerCase();
+
+            if ((userChoice == 'rock') || (userChoice == 'paper') || (userChoice == 'scissors')){
+                break;
+            }
+            console.warn("Please make sure your input is either rock, paper or scissors!");
+        }
+        let computerChoice = getComputerChoice();
+        let result = playSingleRound(userChoice, computerChoice);
+
+        if (result == 'W'){
+            console.log(`You Win! ${userChoice} beats ${computerChoice}`);
+            playerScore++;
+        }
+        if (result == 'L'){
+            console.log(`You Lose! ${computerChoice} beats ${userChoice}`);
+            computerScore++;
+        }
+        else {
+            console.log('Draw!');
+        }
+    }
+    console.log(`scores -> user:${playerScore} computer:${computerScore}`);
 }
 
-function playSingleRound(playerSelection, computerSelection){
-    p = playerSelection.toLowerCase()
-    c = computerSelection.toLowerCase()
-
-    if (p == c){
-        console.log("Draw!")
-    }
-    else if ((p == 'rock' && c == 'paper') || (p == 'paper' && c == 'scissors') || (p == 'scissors' && c == 'rock')){
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
-    }
-    else{
-        return `You Win! ${playerSelection} beats ${computerSelection}`
-    }
-}
+game()
