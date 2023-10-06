085c944d87df6f5da52b2431b2f5542fa218be1c
jahid <jahid1o@outlook.com>
2023-10-06 14:32:03 +0600

    Modufy js/index.js  - added playSingleRound function  - which has playerSelection and computerSekection as function parameters  - and returns   - `You Lose! ${computerSelection} beats ${playerSelection}`   or,   - `You Win! ${playerSelection} beats ${computerSelection}`   based on the rock paper scissors game rules

--- js/index.js
+++ js/index.js
@@ -3,3 +3,18 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)
     return choices[choice]
 }
+
+function playSingleRound(playerSelection, computerSelection){
+    p = playerSelection.toLowerCase()
+    c = computerSelection.toLowerCase()
+
+    if (p == c){
+        console.log("Draw!")
+    }
+    else if ((p == 'rock' && c == 'paper') || (p == 'paper' && c == 'scissors') || (p == 'scissors' && c == 'rock')){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+    else{
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+}
