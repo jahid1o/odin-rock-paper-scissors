0b8d4af16c5147657494b54163f051a22738719b
jahid <jahid1o@outlook.com>
2023-10-16 00:48:21 +0600

    Modified index.html and js/index.js  - added UI to chose either rock, paper or scissors    instead of alerting user for a choice

--- js/index.js
+++ js/index.js
@@ -1,54 +1,59 @@
+const container = document.querySelector("#container")
+const gameRoundSpan = document.querySelector("#game-round-count")
+const userScoreSpan = document.querySelector("#user-score")
+const computerScoreSpan = document.querySelector("#computer-score")
+const playerChoices = document.querySelectorAll(".choice")
+const resultOutput = document.querySelector("#result")
 
-function game(){
-    function getComputerChoice() {
-        let choices = ['Rock', 'Paper', 'Scissors'];
-        let choice = Math.floor(Math.random() * 3);
-        return choices[choice];
-    }
-    
-    function playSingleRound(playerSelection, computerSelection){
-        p = playerSelection.toLowerCase();
-        c = computerSelection.toLowerCase();
-    
-        if (p == c){
-            return 'D';
-        }
-        else if ((p == 'rock' && c == 'paper') || (p == 'paper' && c == 'scissors') || (p == 'scissors' && c == 'rock')){
-            return 'L';
-        }
-        else{
-            return 'W';
-        }
-    }
-    
-    let playerScore = 0, computerScore = 0;
+let round = 1, userScore = 0, computerScore = 0
 
-    for (let round = 0; round < 5; round++){
-        let userChoice;
-        while (true) {
-            userChoice = prompt(`Round ${round+1}.\nYour turn. Chose either rock, paper or scissors.`).toLocaleLowerCase();
-
-            if ((userChoice == 'rock') || (userChoice == 'paper') || (userChoice == 'scissors')){
-                break;
-            }
-            console.warn("Please make sure your input is either rock, paper or scissors!");
-        }
-        let computerChoice = getComputerChoice();
-        let result = playSingleRound(userChoice, computerChoice);
-
-        if (result == 'W'){
-            console.log(`You Win! ${userChoice} beats ${computerChoice}`);
-            playerScore++;
-        }
-        if (result == 'L'){
-            console.log(`You Lose! ${computerChoice} beats ${userChoice}`);
-            computerScore++;
-        }
-        else {
-            console.log('Draw!');
-        }
-    }
-    console.log(`scores -> user:${playerScore} computer:${computerScore}`);
+function getComputerChoice() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choice = Math.floor(Math.random() * 3);
+    return choices[choice];
 }
 
-game()
+function playSingleRound(playerSelection, computerSelection){
+    p = playerSelection.toLowerCase();
+    c = computerSelection.toLowerCase();
+
+    if (p == c){
+        return 'D';
+    }
+    else if ((p == 'rock' && c == 'paper') || (p == 'paper' && c == 'scissors') || (p == 'scissors' && c == 'rock')){
+        return 'L';
+    }
+    else{
+        return 'W';
+    }
+}
+
+playerChoices.forEach(choice => {
+    choice.addEventListener("click", event => {
+        if (userScore > 4) {
+            resultOutput.textContent = "Congratulations you beat the computer in Rock Paper Scissors!"
+            resultOutput.style.color = "red"
+        } 
+        else if (computerScore > 4) {
+            resultOutput.textContent = "You just lost to a computer in Rock Paper Scissors!"
+            resultOutput.style.color = "red"
+        } 
+        else {
+            const result = playSingleRound(event.target.id, getComputerChoice())
+
+            if (result == "W") {
+                resultOutput.textContent = `Round Won!`
+                userScore++
+            } else if (result == "L") {
+                resultOutput.textContent = `Round Lost!`
+                computerScore++
+            } else {
+                resultOutput.textContent = "Draw!"
+            }
+            round++
+            userScoreSpan.textContent = userScore
+            computerScoreSpan.textContent = computerScore
+            gameRoundSpan.textContent = round
+        }
+    })
+})
